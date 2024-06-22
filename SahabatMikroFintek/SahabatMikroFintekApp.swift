bfdb6ce4e47224184ad25d9bd7e2a88b4b6eab58
Kevin Dallian <kevindallian@gmail.com>
2024-06-22 15:21:57 +0700

    Initial Commit

---
+++ SahabatMikroFintek/SahabatMikroFintekApp.swift
@@ -0,0 +1,17 @@
+//
+//  SahabatMikroFintekApp.swift
+//  SahabatMikroFintek
+//
+//  Created by Kevin Dallian on 22/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SahabatMikroFintekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
